55a961bb8f6b303a89ff76f5776b24d4444fc136
Dan Burke <dburke@addictmud.org>
2016-11-18 17:45:28 -0500

    process message

--- lib/Queue/Parasite/Message/webhook.pm
+++ lib/Queue/Parasite/Message/webhook.pm
@@ -1,11 +1,14 @@
 package Queue::Parasite::Message::webhook;
 
 use Moose::Role;
+use LWP::UserAgent;
 
-has webhook => ( is => 'rw', isa => 'Str' );
-has pass    => ( is => 'rw', isa => 'Str' );
-has fail    => ( is => 'rw', isa => 'Str' );
 
+has url  => ( is => 'rw', isa => 'Str' );
+has pass => ( is => 'rw', isa => 'Str' );
+has fail => ( is => 'rw', isa => 'Str' );
+
+has ua => ( is => 'rw', isa => 'LWP::UserAgent', builder => '_builder_ua' );
 
 sub parse {
     my ($self) = @_;
@@ -13,12 +16,12 @@ sub parse {
     my $body = $self->decoded_body;
 
     if (ref($body->{webhook}) eq 'HASH') {
-        $self->webhook($body->{webhook}{url});
+        $self->url($body->{webhook}{url});
         $self->pass($body->{webhook}{pass});
         $self->fail($body->{webhook}{fail});
     }
     else {
-        $self->webhook($body->{webhook});
+        $self->url($body->{webhook});
     }
 
 }
@@ -27,7 +30,36 @@ sub parse {
 sub process {
     my ($self) = @_;
 
+    my $ua = $self->ua;
 
+    # other method support forthcoming
+    my $response = $ua->get( $self->url );
+
+    if ($response->is_success) {
+        if ($self->pass) {
+            my $pass_response = $ua->get( $self->pass );
+            die $pass_response->decoded_content unless $pass_response->is_success;
+        }
+    }
+    elsif ($self->fail) {
+        my $fail_response = $ua->get( $self->fail );
+        die $fail_response->decoded_content unless $fail_response->is_success;
+    }
+    else {
+        die $response->decoded_content unless $response->is_success;
+    }
+
+}
+
+
+sub _builder_ua { 
+    my ($self) = @_;
+
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout(10);
+    $ua->env_proxy;
+
+    $ua;
 }
 
 
